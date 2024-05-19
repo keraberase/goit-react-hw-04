c51898b2f663fe15570fb6cd11e6be23d31c2149
keraberas <46618649+keraberase@users.noreply.github.com>
2024-05-19 18:28:43 +0300

    create project

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import css from "./LoadMoreBtn.module.scss";
+
+interface LoadMoreBtnProps {
+  onLoadMore: () => void;
+}
+
+const LoadMoreBtn: React.FC<LoadMoreBtnProps> = ({ onLoadMore }) => {
+  return (
+    <button className={css.btn} onClick={onLoadMore}>
+      Load More
+    </button>
+  );
+};
+
+export default LoadMoreBtn;
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Circles } from "react-loader-spinner";
 
 const LoaderComponent = () => {
